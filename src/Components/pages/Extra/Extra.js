c98df38f070c9f015f6429db5729c66bfec409b0
unknown <siamur01@gmail.com>
2021-11-12 07:42:46 +0600

    commit

--- src/Components/pages/Extra/Extra.js
+++ src/Components/pages/Extra/Extra.js
@@ -1,18 +1,121 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+
+import * as React from 'react';
+import { Box } from '@mui/system';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import banner1 from './../../../images/banner/banner1.jpg';
+import banner2 from '../../../images/banner/banner2.jpg';
+import banner3 from '../../../images/banner/banner3.jpg';
 
 const Extra = () => {
    return (
-      <Grid
-         container
-         direction="row"
-         justifyContent="center"
-         alignItems="center"
-         padding="30px"
-         backgroundColor="#e0e0e0"
-      >
-         <h2>EXTRA section</h2>
-      </Grid>
+
+      <Box
+         sx={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            p: 1,
+            m: 1,
+            bgcolor: 'background.paper',
+         }}>
+         <Card sx={{ maxWidth: 345, bgcolor: 'background.paper', }}>
+
+            <Box sx={{
+               display: 'flex',
+               justifyContent: 'space-around',
+               p: 1,
+
+               bgcolor: 'background.paper',
+            }}>Up Coming</Box>
+            <CardActionArea>
+               <CardMedia
+                  component="img"
+                  height="140"
+                  image={banner1}
+                  alt="green iguana"
+               />
+               <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                     APIcet 44
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                     Superocean, M17368D71I1S2 BOX PAPERS 2022
+                  </Typography>
+               </CardContent>
+            </CardActionArea>
+            <CardActions>
+               <Button size="small" color="primary">
+                  See more info
+               </Button>
+            </CardActions>
+         </Card>
+         <Card sx={{ maxWidth: 345 }}>
+
+            <Box sx={{
+               display: 'flex',
+               justifyContent: 'space-around',
+               p: 1,
+
+               bgcolor: 'background.paper',
+            }}>Up Coming</Box>
+            <CardActionArea>
+               <CardMedia
+                  component="img"
+                  height="140"
+                  image={banner2}
+                  alt="green iguana"
+               />
+               <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                     Rizard254
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                     Superocean, M17368D71I1S2 BOX PAPERS 2022
+                  </Typography>
+               </CardContent>
+            </CardActionArea>
+            <CardActions>
+               <Button size="small" color="primary">
+                  See more info
+               </Button>
+            </CardActions>
+         </Card>
+         <Card sx={{ maxWidth: 345 }}>
+
+            <Box sx={{
+               display: 'flex',
+               justifyContent: 'space-around',
+               p: 1,
+
+               bgcolor: 'background.paper',
+            }}>Up Coming</Box>
+            <CardActionArea>
+               <CardMedia
+                  component="img"
+                  height="140"
+                  image={banner3}
+                  alt="green iguana"
+               />
+               <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                     HEZARD35F3
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                     Superocean, M17368D71I1S2 BOX PAPERS 2022
+                  </Typography>
+               </CardContent>
+            </CardActionArea>
+            <CardActions>
+               <Button size="small" color="primary">
+                  See more info
+               </Button>
+            </CardActions>
+         </Card>
+      </Box>
+
    );
 };
 
--- src/Components/pages/Review/Review.js
+++ src/Components/pages/Review/Review.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 
+import { Box } from '@mui/system';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
+
+
+
 const Review = () => {
    return (
 
@@ -13,7 +23,40 @@ const Review = () => {
          backgroundColor="#e0e0e0"
 
       >
-         <h2>this is review section</h2>
+
+         <Card sx={{ maxWidth: 345 }}>
+
+            <Box sx={{
+               display: 'flex',
+               justifyContent: 'space-around',
+               p: 1,
+
+               bgcolor: 'background.paper',
+            }}>  <Typography container
+               direction="row"
+               justifyContent="center"
+               alignItems="center"
+               padding="30px"
+               backgroundColor="#e0e0e0"
+               gutterBottom variant="h5" component="div">
+                  WATCH <span style={{ color: '#F05568  ', }}>GALARY</span>
+
+               </Typography></Box>
+            <CardActionArea>
+
+               <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                     Reviews
+                     <StarIcon /><StarIcon /><StarIcon />
+                     <span style={{ color: '#F5468  ', }}>(3.5k)</span>
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                     Find potential problems with the site, if any. When reviewing the site, try to remain unbiased. If you only write reviews about sites that you like, your audience will not appreciate that you only talk about the positive aspects.
+                  </Typography>
+               </CardContent>
+            </CardActionArea>
+
+         </Card>
       </Grid>
    );
 };
